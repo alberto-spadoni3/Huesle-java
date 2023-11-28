0b4d12bccc492acc5739f500cccda6e3d0743345
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-11-28 13:24:45 +0100

    Aggiunta dipendenza per i driver MongoDB

--- server/game/build.gradle.kts
+++ server/game/build.gradle.kts
@@ -7,6 +7,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.mongodb:mongodb-driver-sync:4.11.1")
     implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
