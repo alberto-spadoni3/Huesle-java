794b164d57122d1d66db2af4084484fabc9bf026
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-12 13:21:23 +0100

    Inserite variabili per contenere i vari match

--- server/game/src/main/java/it/unibo/sd/project/mastermind/model/Game.java
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/model/Game.java
@@ -1,4 +1,16 @@
 package it.unibo.sd.project.mastermind.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private final Player player; // the player associated to this game session
+    private List<Math> activeMatches;
+    private List<Math> endedMatches;
+
+    public Game(Player player) {
+        this.player = player;
+        activeMatches = new ArrayList<>();
+        endedMatches = new ArrayList<>();
+    }
 }
