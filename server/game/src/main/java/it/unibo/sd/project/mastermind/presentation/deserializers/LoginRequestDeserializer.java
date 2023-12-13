dbe3a58105a053d3d9aecafb6897003dbe57f954
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-13 13:17:13 +0100

    Aggiunto deserializzatore per LoginRequest e la relativa classe di test

---
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/deserializers/LoginRequestDeserializer.java
@@ -0,0 +1,22 @@
+package it.unibo.sd.project.mastermind.presentation.deserializers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import it.unibo.sd.project.mastermind.model.user.LoginRequest;
+
+public class LoginRequestDeserializer extends AbstractJsonDeserializer<LoginRequest> {
+
+    @Override
+    protected LoginRequest deserializeJson(JsonElement jsonElement) {
+        if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = (JsonObject) jsonElement;
+            if (jsonObject.has("username") && jsonObject.has("password")) {
+                String username = jsonObject.get("username").getAsString();
+                String clearPassword = jsonObject.get("password").getAsString();
+                return new LoginRequest(username, clearPassword);
+            } else
+                throw new RuntimeException("Cannot deserialize as " + LoginRequest.class.getName());
+        } else
+            throw new RuntimeException("Cannot deserialize as " + LoginRequest.class.getName());
+    }
+}
---
+++ server/game/src/test/java/it/unibo/sd/project/mastermind/presentation/LoginRequestTests.java
@@ -0,0 +1,25 @@
+package it.unibo.sd.project.mastermind.presentation;
+
+import it.unibo.sd.project.mastermind.model.user.LoginRequest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginRequestTests {
+    @Test
+    void loginRequestDeserialization() throws Exception {
+        String username = "marior";
+        String clearPassword = "Mario123!";
+        LoginRequest deserializedRequest = Presentation.deserializeAs(
+                getLoginRequestAsJson(username, clearPassword),
+                LoginRequest.class
+        );
+
+        assertEquals(username, deserializedRequest.getUsername());
+        assertEquals(clearPassword, deserializedRequest.getClearPassword());
+    }
+
+    private String getLoginRequestAsJson(String username, String clearPassword) {
+        return "{\"username\":" + username + "," +
+                "\"password\":" + clearPassword + "}";
+    }
+}
