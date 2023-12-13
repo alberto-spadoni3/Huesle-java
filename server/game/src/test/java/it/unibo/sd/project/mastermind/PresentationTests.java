ed2e5f533a77139b62d9cb3c701a1822d58ca486
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-13 12:53:10 +0100

    Aggiunta classe per testare i serializzatori/deserializzatori

---
+++ server/game/src/test/java/it/unibo/sd/project/mastermind/PresentationTests.java
@@ -0,0 +1,105 @@
+package it.unibo.sd.project.mastermind;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import it.unibo.sd.project.mastermind.model.AccessibilitySettings;
+import it.unibo.sd.project.mastermind.model.Player;
+import it.unibo.sd.project.mastermind.presentation.Presentation;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PresentationTests {
+    private final String email = "mario.rossi@unibo.it";
+    private final String username = "marior";
+    private final String clearPassword = "Mario123!";
+    private final String hashedPassword = getHashedPassword(clearPassword);
+    private final byte profilePictureID = 4;
+    private final boolean darkMode = false;
+    private final boolean colorblindMode = true;
+
+    @Test
+    void newPlayerSerialization() {
+        Player newPlayer = new Player(username, email, clearPassword);
+        String serializedPlayer = Presentation.serializerOf(Player.class).serialize(newPlayer);
+        String jsonPlayer = getCustomPlayerAsJson(
+                newPlayer.getPassword(),
+                (byte) 0,
+                true,
+                false
+        );
+
+        assertEquals(jsonPlayer, serializedPlayer);
+    }
+
+    @Test
+    void CustomPlayerSerialization() {
+        Player customPlayer = new Player(
+                username, email, hashedPassword, profilePictureID,
+                new AccessibilitySettings(darkMode, colorblindMode),
+                false
+            );
+        String serializedPlayer = Presentation.serializerOf(Player.class).serialize(customPlayer);
+        String jsonPlayer = getCustomPlayerAsJson(
+                hashedPassword,
+                profilePictureID,
+                darkMode,
+                colorblindMode
+        );
+
+        assertEquals(jsonPlayer, serializedPlayer);
+    }
+
+    @Test
+    void newPlayerDeserialization() throws Exception {
+        Player newPlayer = Presentation.deserializeAs(getNewPlayerAsJson(), Player.class);
+
+        assertEquals(username, newPlayer.getUsername());
+        assertEquals(email, newPlayer.getEmail());
+        assertTrue(newPlayer.verifyPassword(clearPassword));
+        assertEquals(0, newPlayer.getProfilePictureID());
+        assertTrue(newPlayer.getSettings().isDarkMode());
+        assertFalse(newPlayer.getSettings().isColorblindMode());
+        assertFalse(newPlayer.isDisabled());
+    }
+
+    @Test
+    void CustomPlayerDeserialization() throws Exception {
+        Player deserializedPlayer = Presentation.deserializeAs(
+                getCustomPlayerAsJson(
+                        hashedPassword,
+                        profilePictureID,
+                        darkMode,
+                        colorblindMode
+                ), Player.class);
+
+        assertEquals(username, deserializedPlayer.getUsername());
+        assertEquals(email, deserializedPlayer.getEmail());
+        assertEquals(hashedPassword, deserializedPlayer.getPassword());
+        assertEquals(profilePictureID, deserializedPlayer.getProfilePictureID());
+        assertEquals(darkMode, deserializedPlayer.getSettings().isDarkMode());
+        assertEquals(colorblindMode, deserializedPlayer.getSettings().isColorblindMode());
+        assertFalse(deserializedPlayer.isDisabled());
+    }
+
+    private String getHashedPassword(String clearPassword) {
+        return BCrypt.withDefaults().hashToString(14, clearPassword.toCharArray());
+    }
+
+    private String getNewPlayerAsJson() {
+        return "{ \"username\": " + username + "," +
+                "\"email\": " + email + "," +
+                "\"password\": " + clearPassword + "}";
+    }
+
+    private String getCustomPlayerAsJson(String hashedPassword, byte profilePictureID,
+                                         boolean darkMode, boolean colorblindMode) {
+        return "{" +
+                "\"username\":\"" + username + "\"," +
+                "\"email\":\"" + email + "\"," +
+                "\"password\":\"" + hashedPassword + "\"," +
+                "\"profilePictureID\":" + profilePictureID + "," +
+                "\"disabled\":false," +
+                "\"accessibilitySettings\":{" +
+                "\"darkMode\":" + darkMode + "," +
+                "\"colorblindMode\":" + colorblindMode + "}}";
+    }
+}
