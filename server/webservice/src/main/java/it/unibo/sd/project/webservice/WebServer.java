a318619aa250682528fc3992783b8121c4edb238
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-14 18:30:52 +0100

    Inizio implementazione del Web server usando Vertx Web

--- server/webservice/src/main/java/it/unibo/sd/project/webservice/WebServer.java
+++ server/webservice/src/main/java/it/unibo/sd/project/webservice/WebServer.java
@@ -1,4 +1,70 @@
 package it.unibo.sd.project.webservice;
 
-public class WebServer {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.bridge.BridgeEventType;
+import io.vertx.ext.bridge.PermittedOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.sockjs.SockJSBridgeOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import it.unibo.sd.project.webservice.rabbit.MessageType;
+import it.unibo.sd.project.webservice.rabbit.RPCClient;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class WebServer extends AbstractVerticle {
+    private final short LISTENING_PORT;
+    private RPCClient gameBackend;
+    private HttpServer httpServer;
+
+    public WebServer(short listeningPort) {
+        LISTENING_PORT = listeningPort;
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        try {
+            gameBackend = new RPCClient();
+        } catch (IOException | TimeoutException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Router router = Router.router(vertx);
+
+        // TODO manage CORS when needed
+        // router.route().handler(CorsHandler.create().addOrigin("http://localhost"));
+
+        router.route("/eventbus/*").subRouter(getEventBusRouter());
+        router.route("/api/user/*").subRouter(getUserRouter());
+
+        httpServer = vertx
+                .createHttpServer()
+                .requestHandler(router)
+                .listen(LISTENING_PORT, res -> {
+                    if (res.succeeded())
+                        startPromise.complete();
+                    else startPromise.fail(res.cause());
+                });
+    }
+
+    private Router getUserRouter() {
+        Router router = Router.router(vertx);
+        return router;
+    }
+
+    private Router getEventBusRouter() {
+        SockJSBridgeOptions options = new SockJSBridgeOptions();
+        options.addInboundPermitted(new PermittedOptions().setAddressRegex("huesle.*"));
+        options.addOutboundPermitted(new PermittedOptions().setAddressRegex("huesle.*"));
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+        return sockJSHandler.bridge(options, event -> {
+            if (event.type() == BridgeEventType.SOCKET_CREATED) {
+                System.out.println("new socket created " + event);
+            }
+            event.complete(true);
+        });
+    }
 }
