59fa7c8ad1ffc400b8e1f99735f3f79f96df4002
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-14 18:33:56 +0100

    Aggiunte le dipendenze necessarie e definite le variabili d'ambiente

--- server/game/build.gradle.kts
+++ server/game/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.rabbitmq:amqp-client:5.20.0")
     implementation("at.favre.lib:bcrypt:0.10.2")
+    implementation("org.slf4j:slf4j-log4j12:2.0.9")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
 }
@@ -22,10 +23,15 @@ java {
 }
 
 // Task used to run the microservice related to the game
-/*task("game", JavaExec::class) {
-    mainClass.set("it.unibo.sd.project.mastermind.Game")
+task("game", JavaExec::class) {
+    val env = mapOf (
+            "RABBIT_HOST" to "localhost",
+            "MONGO_HOST" to "mongodb://localhost:27017"
+        )
+    environment = env
+    mainClass.set("it.unibo.sd.project.mastermind.Main")
     classpath = sourceSets["main"].runtimeClasspath
-}*/
+}
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
--- server/webservice/build.gradle.kts
+++ server/webservice/build.gradle.kts
@@ -1,12 +1,12 @@
-plugins {
-    java
-}
+plugins { java }
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.vertx:vertx-web:4.4.0")
+    runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.101.Final:osx-x86_64")
     implementation("com.rabbitmq:amqp-client:5.20.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
@@ -19,10 +19,11 @@ java {
 }
 
 // Task used to run the microservice related to the web service
-/*task("web-service", JavaExec::class) {
-    mainClass.set("it.unibo.sd.project.webservice.WebServer")
+task("web-service", JavaExec::class) {
+    environment = mapOf("RABBIT_HOST" to "localhost")
+    mainClass.set("it.unibo.sd.project.webservice.Main")
     classpath = sourceSets["main"].runtimeClasspath
-}*/
+}
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
