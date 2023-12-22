6685ba1462fabd11bb9afc8dfc2ee8655120c394
Agnohendrix <agnohendrix@gmail.com>
2023-12-22 19:42:36 +0100

    Fixed MatchSerializerTest for empty playerList

--- server/game/src/test/java/it/unibo/sd/project/mastermind/presentation/serializers/MatchSerializerTests.java
+++ server/game/src/test/java/it/unibo/sd/project/mastermind/presentation/serializers/MatchSerializerTests.java
@@ -1,2 +1,37 @@
-package it.unibo.sd.project.mastermind.presentation.serializers;public class MatchSerializerTests {
+package it.unibo.sd.project.mastermind.presentation.serializers;
+
+import it.unibo.sd.project.mastermind.model.Attempt;
+import it.unibo.sd.project.mastermind.model.Player;
+import it.unibo.sd.project.mastermind.model.SecretCode;
+import it.unibo.sd.project.mastermind.model.match.Match;
+import it.unibo.sd.project.mastermind.model.match.MatchStatus;
+import it.unibo.sd.project.mastermind.presentation.Presentation;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatchSerializerTests {
+    UUID uuid = new UUID(1, 2);
+    Player p1 = new Player("prova","pro@pro.va","asdf");
+    Player p2 = new Player("p2", "p2@pro.va","asdf");
+    ArrayList<Player> players = new ArrayList<Player>();
+
+    ArrayList listAttempts = new ArrayList<Attempt>();
+
+    SecretCode secretCode = new SecretCode();
+
+    @Test
+    void matchSerializerTest(){
+        players.add(p1);
+        players.add(p2);
+        MatchStatus matchStatus = new MatchStatus(players);
+        Match match = new Match(uuid, matchStatus, listAttempts, secretCode);
+        System.out.println(Presentation.serializerOf(Match.class).serialize(match));
+
+        assertEquals(true, true);
+    }
 }
