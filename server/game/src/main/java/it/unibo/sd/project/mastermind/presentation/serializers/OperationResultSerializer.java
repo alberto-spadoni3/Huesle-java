1e3f119cb2cc8d01a285b7310a5dd84125ef3954
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-24 14:06:01 +0100

    Classe modificata affinchè abbia al suo interno anche l'accessToken

--- server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/serializers/OperationResultSerializer.java
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/serializers/OperationResultSerializer.java
@@ -2,7 +2,9 @@ package it.unibo.sd.project.mastermind.presentation.serializers;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import it.unibo.sd.project.mastermind.model.Player;
 import it.unibo.sd.project.mastermind.model.user.OperationResult;
+import it.unibo.sd.project.mastermind.presentation.Presentation;
 
 public class OperationResultSerializer extends AbstractJsonSerializer<OperationResult> {
     @Override
@@ -10,6 +12,14 @@ public class OperationResultSerializer extends AbstractJsonSerializer<OperationR
         JsonObject jsonOpResult = new JsonObject();
         jsonOpResult.addProperty("statusCode", object.getStatusCode());
         jsonOpResult.addProperty("resultMessage", object.getResultMessage());
+        Player possibleUser = object.getRelatedUser();
+        String accessToken = object.getAccessToken();
+        if (possibleUser != null && accessToken != null) {
+            JsonElement jsonPlayer = Presentation.serializerOf(Player.class).getJsonElement(possibleUser);
+            if (jsonPlayer.isJsonObject())
+                jsonOpResult.add("relatedUser", jsonPlayer);
+            jsonOpResult.addProperty("accessToken", accessToken);
+        }
         return jsonOpResult;
     }
 }
