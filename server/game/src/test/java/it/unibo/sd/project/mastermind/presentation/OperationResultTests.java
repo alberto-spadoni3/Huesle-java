4766251a6bd090a28a5dd879001d06b1c27e57f5
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-24 14:06:18 +0100

    Test per OperationResult

---
+++ server/game/src/test/java/it/unibo/sd/project/mastermind/presentation/OperationResultTests.java
@@ -0,0 +1,113 @@
+package it.unibo.sd.project.mastermind.presentation;
+
+import it.unibo.sd.project.mastermind.model.AccessibilitySettings;
+import it.unibo.sd.project.mastermind.model.Player;
+import it.unibo.sd.project.mastermind.model.user.OperationResult;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OperationResultTests {
+    @Test
+    void failedOperationResultSerialization() {
+        short statusCode = 400;
+        String resultMessage = "error";
+        OperationResult operationResult = new OperationResult(statusCode, resultMessage);
+        String serializerOpResult = Presentation.serializerOf(OperationResult.class).serialize(operationResult);
+        String expectedResult = getFailedOperationResultAsJson(statusCode, resultMessage);
+        assertEquals(expectedResult, serializerOpResult);
+    }
+
+    @Test
+    void successOperationResultSerialization() {
+        short statusCode = 200;
+        String resultMessage = "success";
+        String accessToken = "an.access.token";
+        String username = "mariello";
+        String hashedPassword = "$2a$05$bvIG6Nmid91Mu9RcmmWZfO5HJIMCT8riNW0hEp8f6/FuA2/mHZFpe";
+        String email = "mario@me.com";
+        String refreshToken = "a.refresh.token";
+        byte profilePictureID = 0;
+        boolean darkMode = true;
+        boolean colorblindMode = true;
+
+        Player player = new Player(username,email, hashedPassword, refreshToken,
+                                   profilePictureID, new AccessibilitySettings(darkMode, colorblindMode), false);
+        OperationResult operationResult = new OperationResult(statusCode, resultMessage, player, accessToken);
+        String serializerOpResult = Presentation.serializerOf(OperationResult.class).serialize(operationResult);
+
+        String jsonPlayer = getCustomPlayerAsJson(username, email, hashedPassword, refreshToken,
+                profilePictureID, darkMode, colorblindMode);
+        String expectedResult = getSuccessOperationResultAsJson(statusCode, resultMessage, jsonPlayer, accessToken);
+        assertEquals(expectedResult, serializerOpResult);
+    }
+
+    @Test
+    void failedOperationResultDeserialization() throws Exception {
+        short statusCode = 400;
+        String resultMessage = "error";
+
+        OperationResult deserialized = Presentation.deserializeAs(
+                getFailedOperationResultAsJson(statusCode, resultMessage),
+                OperationResult.class
+        );
+
+        assertEquals(statusCode, deserialized.getStatusCode());
+        assertEquals(resultMessage, deserialized.getResultMessage());
+    }
+
+    @Test
+    void successOperationResultDeserialization() throws Exception {
+        short statusCode = 200;
+        String resultMessage = "success";
+        String accessToken = "an.access.token";
+        String username = "mariello";
+        String hashedPassword = "$2a$05$bvIG6Nmid91Mu9RcmmWZfO5HJIMCT8riNW0hEp8f6/FuA2/mHZFpe";
+        String email = "mario@me.com";
+        String refreshToken = "a.refresh.token";
+        byte profilePictureID = 0;
+        boolean darkMode = true;
+        boolean colorblindMode = true;
+
+        String jsonPlayer = getCustomPlayerAsJson(username, email, hashedPassword, refreshToken,
+                profilePictureID, darkMode, colorblindMode);
+        OperationResult deserialized = Presentation.deserializeAs(
+                getSuccessOperationResultAsJson(statusCode, resultMessage, jsonPlayer, accessToken),
+                OperationResult.class
+        );
+        assertEquals(statusCode, deserialized.getStatusCode());
+        assertEquals(resultMessage, deserialized.getResultMessage());
+        Player relatedUser = deserialized.getRelatedUser();
+        assertEquals(username, relatedUser.getUsername());
+        assertEquals(email, relatedUser.getEmail());
+        assertEquals(refreshToken, relatedUser.getRefreshToken());
+        assertEquals(darkMode, relatedUser.getSettings().isDarkMode());
+    }
+
+    private String getFailedOperationResultAsJson(short statusCode, String resultMessage) {
+        return "{\"statusCode\":" + statusCode + "," +
+                "\"resultMessage\":\"" + resultMessage + "\"}";
+    }
+
+    private String getSuccessOperationResultAsJson(short statusCode, String resultMessage, String player, String accessToken) {
+        return "{\"statusCode\":" + statusCode + "," +
+                "\"resultMessage\":\"" + resultMessage + "\"," +
+                "\"relatedUser\":" + player + "," +
+                "\"accessToken\":\"" + accessToken + "\"}";
+    }
+
+    private String getCustomPlayerAsJson(String username, String email,
+                                         String hashedPassword, String refreshToken, byte profilePictureID,
+                                         boolean darkMode, boolean colorblindMode) {
+        return "{" +
+                "\"username\":\"" + username + "\"," +
+                "\"email\":\"" + email + "\"," +
+                "\"password\":\"" + hashedPassword + "\"," +
+                "\"refreshToken\":\"" + (refreshToken == null ? "" : refreshToken) + "\"," +
+                "\"profilePictureID\":" + profilePictureID + "," +
+                "\"disabled\":false," +
+                "\"accessibilitySettings\":{" +
+                "\"darkMode\":" + darkMode + "," +
+                "\"colorblindMode\":" + colorblindMode + "}}";
+    }
+}
