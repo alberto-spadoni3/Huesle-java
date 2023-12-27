2beccf53d4431731a394f006ce1233f814ce300e
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2023-12-27 17:24:25 +0000

    Started working on MatchSerializer

---
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/deserializers/MatchDeserializer.java
@@ -0,0 +1,35 @@
+package it.unibo.sd.project.mastermind.presentation.deserializers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import it.unibo.sd.project.mastermind.model.Player;
+import it.unibo.sd.project.mastermind.model.match.MatchState;
+import it.unibo.sd.project.mastermind.model.match.MatchStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchDeserializer extends AbstractJsonDeserializer<Match> {
+    @Override
+    protected Match deserializeJson(JsonElement jsonElement) {
+        if (jsonElement.isJsonObject()) {
+            JsonObject result = (JsonObject) jsonElement;
+            MatchState matchState = MatchState.valueOf(result.get("UUID").getAsString());
+            List<Player> players = new ArrayList<>();
+            JsonObject jsonPlayers = result.getAsJsonObject("players");
+            PlayerDeserializer ps = new PlayerDeserializer();
+            System.out.println(jsonPlayers.size());
+            for(int i = 0; i < jsonPlayers.size(); i++){
+                players.add(ps.deserializeJson(jsonPlayers.get(String.valueOf(i))));
+            }
+            JsonObject jsonNextPlayer = result.getAsJsonObject("nextPlayer");
+            Player nextPlayer = ps.deserializeJson(jsonNextPlayer);
+            MatchStatus ms = new MatchStatus(players);
+            ms.changeNextPlayer(nextPlayer);
+            ms.changeState(matchState);
+            return ms;
+        } else {
+            throw new RuntimeException("Cannot deserialize " + jsonElement + " as Match");
+        }
+    }
+}
