0bdab264e8bc3ecb91070d5a68889da750f250b5
Agnohendrix <agnohendrix@gmail.com>
2023-12-28 01:29:05 +0100

    Working on SecretCodeDeserializer

--- server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/deserializers/SecretCodeDeserializer.java
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/deserializers/SecretCodeDeserializer.java
@@ -1,2 +1,22 @@
-package it.unibo.sd.project.mastermind.presentation.deserializers;public class SecretCodeDeserializer {
+package it.unibo.sd.project.mastermind.presentation.deserializers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import it.unibo.sd.project.mastermind.model.SecretCode;
+
+import java.util.ArrayList;
+
+public class SecretCodeDeserializer extends AbstractJsonDeserializer<SecretCode> {
+    @Override
+    protected SecretCode deserializeJson(JsonElement jsonElement) {
+        if (jsonElement.isJsonObject()) {
+            JsonObject result = (JsonObject) jsonElement;
+            ArrayList<String> sequence = new ArrayList<>();
+            //TODO After modifying serializer with JSONArray
+
+            return new SecretCode(sequence);
+        } else {
+            throw new RuntimeException("Cannot deserialize " + jsonElement + " as SecretCode");
+        }
+    }
 }
