88b3324865db2bcf1afc7b27f83356098e22aa30
Agnohendrix <agnohendrix@gmail.com>
2023-12-28 02:10:50 +0100

    Cleaned code

--- server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/serializers/SecretCodeSerializer.java
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/presentation/serializers/SecretCodeSerializer.java
@@ -9,13 +9,6 @@ public class SecretCodeSerializer extends AbstractJsonSerializer<SecretCode> {
     @Override
     protected JsonElement toJsonElement(SecretCode secretCode) {
         JsonObject jsonSecretCode = new JsonObject();
-        String stringSecretCode = new String();
-        for(String s : secretCode.getCode()){
-            stringSecretCode += s;
-            if(!s.equals(secretCode.getCode().get(secretCode.getCOLOR_SEQUENCE_LENGTH() - 1))){
-                stringSecretCode += ",";
-            }
-        }
         JsonArray jsonColorCode = new JsonArray();
         for(String s : secretCode.getCode()){
             jsonColorCode.add(s);
