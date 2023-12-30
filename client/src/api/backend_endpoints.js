78f58a7ea0fc3d696a14d0f6d0b2732a220c55e9
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2023-12-30 13:21:36 +0100

    Modificati gli URL in modo da rispecchiare quelli previsti dal server

--- client/src/api/backend_endpoints.js
+++ client/src/api/backend_endpoints.js
@@ -1,30 +1,33 @@
 //User Management
 export const BACKEND_LOGIN_ENDPOINT = "/user/login";
-export const BACKEND_SETTINGS_ENDPOINT = "/setting/profileSetting";
+export const BACKEND_SETTINGS_ENDPOINT = "/protected/settings/profileSetting";
 export const BACKEND_REGISTRATION_ENDPOINT = "/user/register";
-export const BACKEND_UPDATE_USERNAME = "/setting/updateUsername";
-export const BACKEND_UPDATE_EMAIL = "/setting/updateEmail";
-export const BACKEND_UPDATE_PASSWORD_ENDPOINT = "/setting/updatePassword";
+export const BACKEND_UPDATE_USERNAME = "/protected/settings/updateUsername";
+export const BACKEND_UPDATE_EMAIL = "/protected/settings/updateEmail";
+export const BACKEND_UPDATE_PASSWORD_ENDPOINT =
+  "/protected/settings/updatePassword";
 export const BACKEND_REFRESH_TOKEN_ENDPOINT = "/user/refreshToken";
 export const BACKEND_LOGOUT_ENDPOINT = "/user/logout";
 export const BACKEND_DELETE_USER_ENDPOINT = "/user/delete";
 
 //Match Search
-export const BACKEND_SEARCH_MATCH_ENDPOINT = "/search/searchMatch";
-export const BACKEND_JOIN_PRIVATE_MATCH_ENDPOINT = "/search/joinPrivateMatch";
+export const BACKEND_SEARCH_MATCH_ENDPOINT = "/protected/search/searchMatch";
+export const BACKEND_JOIN_PRIVATE_MATCH_ENDPOINT =
+  "/protected/search/joinPrivateMatch";
 
 //Game Moves
-export const BACKEND_GET_MATCH_ENDPOINT = "/game/getMatch";
-export const BACKEND_DO_GUESS_ENDPOINT = "/game/doGuess";
-export const BACKEND_LEAVE_MATCH_ENDPOINT = "/game/leaveMatch";
+export const BACKEND_GET_MATCH_ENDPOINT = "/protected/game/getMatch";
+export const BACKEND_DO_GUESS_ENDPOINT = "/protected/game/doGuess";
+export const BACKEND_LEAVE_MATCH_ENDPOINT = "/protected/game/leaveMatch";
 
 //Socket
 export const BACKEND_SOCKET_ENDPOINT = "http://localhost:8081";
 
 //Stats Management
-export const BACKEND_GET_MATCHES_ENDPOINT = "/stats/allMatches";
-export const BACKEND_UPDATE_USERPIC_ENDPOIND = "/setting/profilePics";
-export const BACKEND_GET_USER_STATS_ENDPOINT = "/stats/userStats";
+export const BACKEND_GET_MATCHES_ENDPOINT = "/protected/stats/allMatches";
+export const BACKEND_UPDATE_USERPIC_ENDPOIND =
+  "/protected/settings/profilePics";
+export const BACKEND_GET_USER_STATS_ENDPOINT = "/protected/stats/userStats";
 
 //Password Reset
 export const BACKEND_FORGOT_PASSWORD_ENDPOINT = "/user/forgotPassword";
@@ -32,5 +35,6 @@ export const BACKEND_CHECK_REQUEST_TOKEN_ENDPOINT = "/user/checkRequestToken";
 export const BACKEND_RESET_PASSWORD_ENDPOINT = "/user/resetPassword";
 
 //Notifications
-export const BACKEND_NOTIFICATIONS_ENDPOINT = "/stats/notifications";
-export const BACKEND_NEW_NOTIFICATIONS_ENDPOINT = "/stats/newNotifications";
+export const BACKEND_NOTIFICATIONS_ENDPOINT = "/protected/stats/notifications";
+export const BACKEND_NEW_NOTIFICATIONS_ENDPOINT =
+  "/protected/stats/newNotifications";
