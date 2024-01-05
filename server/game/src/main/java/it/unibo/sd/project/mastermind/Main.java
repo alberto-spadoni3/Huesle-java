7f70149dc73609e6ebee98ad9e84f8ee69ce2333
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-01-05 11:59:15 +0100

    Main modificato in modo che avvii anche il gameManager

--- server/game/src/main/java/it/unibo/sd/project/mastermind/Main.java
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/Main.java
@@ -1,9 +1,11 @@
 package it.unibo.sd.project.mastermind;
 
+import it.unibo.sd.project.mastermind.model.GameManager;
 import it.unibo.sd.project.mastermind.model.user.UserManager;
 
 public class Main {
     public static void main(String[] args) {
         UserManager userManager = new UserManager();
+        GameManager gameManager = new GameManager();
     }
 }
