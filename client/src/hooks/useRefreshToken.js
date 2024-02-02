219735fbe47b5fbfbbd98e053c1e26a12408ca5e
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-02-02 12:02:38 +0100

    Prima di effettuare il refresh del token JWT, viene chiusa la socket corrente per evitare connessioni multiple, di cui una non verrebbe più utilizzata

--- client/src/hooks/useRefreshToken.js
+++ client/src/hooks/useRefreshToken.js
@@ -1,14 +1,20 @@
 import axios from "../api/axios";
 import useAuth from "./useAuth";
-import { BACKEND_REFRESH_TOKEN_ENDPOINT } from "../api/backend_endpoints";
+import useSocket from "./useSocket";
+import {BACKEND_REFRESH_TOKEN_ENDPOINT} from "../api/backend_endpoints";
 
 const useRefreshToken = () => {
     const { setAuth } = useAuth();
+    const { closeSocket } = useSocket();
 
-    const refresh = async () => {
+    return async () => {
+        // Before setting a new auth object, we close the current socket
+        closeSocket();
+
         const response = await axios.get(BACKEND_REFRESH_TOKEN_ENDPOINT, {
             withCredentials: true,
         });
+
         await setAuth({
             username: response.data?.username,
             accessToken: response.data?.newAccessToken,
@@ -17,8 +23,6 @@ const useRefreshToken = () => {
         });
         return response.data.newAccessToken;
     };
-
-    return refresh;
 };
 
 export default useRefreshToken;
