9c66c8c8bc19ba3e945d422856481959eff0ba28
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-06-12 10:09:09 +0200

    Correzione minore a un commento

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,6 +3,6 @@ extra["environmentVariables"] = mapOf(
         "MONGO_CONN" to (System.getenv("MONGO_CONN") ?: "mongodb://localhost:27017"),
         "ACCESS_TOKEN_SECRET" to (System.getenv("ACCESS_TOKEN_SECRET") ?: "replace with a real passworD"),
         "REFRESH_TOKEN_SECRET" to (System.getenv("REFRESH_TOKEN_SECRET") ?: "Replace with a real password"),
-        "ACCESS_TOKEN_EXPIRATION" to (System.getenv("ACCESS_TOKEN_EXPIRATION")?.toIntOrNull() ?: 20), // 2 minutes
+        "ACCESS_TOKEN_EXPIRATION" to (System.getenv("ACCESS_TOKEN_EXPIRATION")?.toIntOrNull() ?: 20), // 20 minutes
         "REFRESH_TOKEN_EXPIRATION" to (System.getenv("REFRESH_TOKEN_EXPIRATION")?.toIntOrNull() ?: 1440) // 24 hours
 )
