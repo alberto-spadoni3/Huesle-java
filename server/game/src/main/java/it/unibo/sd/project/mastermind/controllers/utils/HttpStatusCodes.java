1a59a8c4ed2877e569b72296d0e14c2e12393395
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2024-06-17 19:28:48 +0200

    Aggiunta classe con i vari codici di stato HTTP utilizzati

---
+++ server/game/src/main/java/it/unibo/sd/project/mastermind/controllers/utils/HttpStatusCodes.java
@@ -0,0 +1,13 @@
+package it.unibo.sd.project.mastermind.controllers.utils;
+
+public class HttpStatusCodes {
+    public static final short OK = 200;
+    public static final short CREATED = 201;
+    public static final short NO_CONTENT = 204;
+    public static final short BAD_REQUEST = 400;
+    public static final short UNAUTHORIZED = 401;
+    public static final short FORBIDDEN = 403;
+    public static final short NOT_FOUND = 404;
+    public static final short CONFLICT = 409;
+}
+
---
+++ server/webservice/src/main/java/it/unibo/sd/project/webservice/configuration/utils/HttpStatusCodes.java
@@ -0,0 +1,11 @@
+package it.unibo.sd.project.webservice.configuration.utils;
+
+public class HttpStatusCodes {
+    public static final short OK = 200;
+    public static final short CREATED = 201;
+    public static final short NO_CONTENT = 204;
+    public static final short BAD_REQUEST = 400;
+    public static final short UNAUTHORIZED = 401;
+    public static final short FORBIDDEN = 403;
+    public static final short INTERNAL_SERVER_ERROR = 500;
+}
